92f2adb3af81eb827a97be863fc4a1ba549e58d0
Tim Kurvers <tim@moonsphere.net>
2014-06-03 23:01:14 +0200

    Correct indentation

--- dist/jsbn.js
+++ dist/jsbn.js
@@ -2585,51 +2585,51 @@ function bit_rol(num, cnt)
 }
 
 
-    // Hooks
-    BigInteger.fromInt = nbv;
+  // Hooks
+  BigInteger.fromInt = nbv;
 
-    // JSBN API
-    return {
-        crypto: {
-            ec: {
-                ECFieldElementFp: ECFieldElementFp,
-                ECPointFp: ECPointFp,
-                ECCurveFp: ECCurveFp
-            },
-            sec: {
-                byName: getSECCurveByName,
-                X9ECParameters: X9ECParameters
-            },
-            hash: {
-                hmac: {
-                    sha1: {
-                        fromStrings: hex_hmac_sha1,
-                        fromArrays: ba_hmac_sha1
-                    }
-                },
-                sha1: {
-                    fromString: hex_sha1,
-                    fromArray: ba_sha1
-                }
-            },
-            prng: {
-                ARC4: Arcfour,
-                SecureRandom: SecureRandom
-            },
-            rsa: {
-                RSAKey: RSAKey
-            }
+  // JSBN API
+  return {
+    crypto: {
+      ec: {
+        ECFieldElementFp: ECFieldElementFp,
+        ECPointFp: ECPointFp,
+        ECCurveFp: ECCurveFp
+      },
+      sec: {
+        byName: getSECCurveByName,
+        X9ECParameters: X9ECParameters
+      },
+      hash: {
+        hmac: {
+          sha1: {
+            fromStrings: hex_hmac_sha1,
+            fromArrays: ba_hmac_sha1
+          }
         },
-        math: {
-            BigInteger: BigInteger
-        },
-        util: {
-            base64: {
-                fromHex: hex2b64,
-                toHex: b64tohex,
-                toByteArray: b64toBA
-            }
+        sha1: {
+          fromString: hex_sha1,
+          fromArray: ba_sha1
         }
-    };
+      },
+      prng: {
+        ARC4: Arcfour,
+        SecureRandom: SecureRandom
+      },
+      rsa: {
+        RSAKey: RSAKey
+      }
+    },
+    math: {
+      BigInteger: BigInteger
+    },
+    util: {
+      base64: {
+        fromHex: hex2b64,
+        toHex: b64tohex,
+        toByteArray: b64toBA
+      }
+    }
+  };
 
 }(this));
